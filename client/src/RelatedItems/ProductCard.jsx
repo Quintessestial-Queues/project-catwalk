d92e7e7d454c3e8ae23bd929f3b400dd6848f00a
Zadkiel Castaneda <castzad@gmail.com>
2021-04-15 20:46:13 -0700

    Set up structure of Product card

---
+++ client/src/RelatedItems/ProductCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      {console.log('product card loaded')}
+    </div>
+  )
+}
+
+export default ProductCard;
---
+++ client/src/RelatedItems/RelatedProductCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductCard from './ProductCard.jsx'
+
+const RelatedProductCards = () => {
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  )
+};
+
+export default RelatedProductCards;
