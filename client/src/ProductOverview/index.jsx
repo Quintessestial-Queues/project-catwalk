eaf2830abf17d9f3a18f0bd71524b0522a169765
Leslie Rodriguez <rleslie1015@gmail.com>
2021-04-16 22:24:50 -0500

    brings in more dummy data for product styles

--- client/src/ProductOverview/index.jsx
+++ client/src/ProductOverview/index.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import StarRating from '../SharedComponents/StarRating.jsx';
 import styles from './product.module.css';
+import { dummyProduct } from '../dummyData.js'
 
 function ProductOverview() {
+  const [product, setProduct] = useState({});
 
+  useEffect(() => {
+    setProduct(dummyProduct);
+  }, [])
 
   return (
     <div className={styles.grid}>
@@ -11,17 +16,17 @@ function ProductOverview() {
       <div className={styles.item}>
         <StarRating
           starRating={1.6} // star rating from api will be integer
-          />
-          <h1> product title </h1>
-          <div>style selection</div>
+        />
+        <h1> product title </h1>
+        <div>style selection</div>
       </div>
 
       <div className={`${styles.item} ${styles.description}`}>
-      <div>
-        Product Description and information
+        <div>
+          Product Description and information
       </div>
-      <div>
-        Product Description and information
+        <div>
+          Product Description and information
       </div>
       </div>
     </div>
