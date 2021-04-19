06fb91bd6bc514b278d1e14e8eef1233a1d6a761
Zadkiel Castaneda <castzad@gmail.com>
2021-04-19 10:28:58 -0700

    Working on product info

--- client/src/RelatedItems/ProductCard.jsx
+++ client/src/RelatedItems/ProductCard.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import ProductInfo from './ProductInfo.jsx';
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className='product-card'>
       {console.log('product card loaded')}
+      {/* <button className='action-button' /> */}
+      <div className='preview-image'>
+
+      </div>
+      <div className='product-info'>
+        <ProductInfo />
+      </div>
     </div>
   )
-}
+};
 
 export default ProductCard;
--- client/src/RelatedItems/index.jsx
+++ client/src/RelatedItems/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './relatedItems.module.css';
+import RelatedProductCards from './RelatedProductCards.jsx'
+import sampleData from './sampleData.js'
 
 function RelatedItems () {
-
-  return (<div> Related Items and Comparisons </div>)
+  return (
+    <div> Related Items and Comparisons
+      <RelatedProductCards />
+    </div>
+  )
 };
 
 export default RelatedItems;
