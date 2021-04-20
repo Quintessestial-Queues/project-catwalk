148c16495c26fafb4b2d35a6e6e2f1b393ef67c8
andrew lee <andrewjetlee777@gmail.com>
2021-04-20 12:10:17 -0700

    Added StarRating Component to Review Item and the date the review was written; Added moment.js as a dependency

--- client/src/RatingsAndReviews/Reviews/ReviewItem.jsx
+++ client/src/RatingsAndReviews/Reviews/ReviewItem.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import StarRating from '../../SharedComponents/StarRating.jsx';
+import moment from 'moment';
 
 //Styling
 import styles from './ReviewItem.module.css'
 
-let ReviewItem = (props) => {
+let ReviewItem = ({review}) => {
+
+  let rating = review.rating;
+
   return (
     <div className={styles.reviewContainer}>
-      Review
+      <span className={styles.starRating}><StarRating starRating={rating}/></span>
+      <span className={styles.datePosted}>{moment(review.date).format("ll")}</span>
     </div>
   )
 }
