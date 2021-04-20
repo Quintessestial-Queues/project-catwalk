40be9e34cdc3d6ae70ca47660e7d442f11e97c66
andrew lee <andrewjetlee777@gmail.com>
2021-04-20 14:59:42 -0700

    Added a 'I recommend this product' text and checkmark icon if the user has recommended the product in the reviewItem component

--- client/src/RatingsAndReviews/Reviews/ReviewItem.jsx
+++ client/src/RatingsAndReviews/Reviews/ReviewItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import StarRating from '../../SharedComponents/StarRating.jsx';
 import moment from 'moment';
+import { FaCheck } from 'react-icons/fa';
 
 //Styling
 import styles from './ReviewItem.module.css'
@@ -9,18 +10,20 @@ let ReviewItem = ({review}) => {
 
   let rating = review.rating;
   let boldedSummary = review.summary.slice(0, 60);
-  //TODO: Bugg with rest of summary being on the same row as the boldedSummary; Try to get the rest of the summary under the boldedSummary
   let restOfSummary = review.summary.length < 60 ? null : `...${review.summary.slice(60)}`
   let body = review.body.slice(0, 250);
+  let checkIcon = <FaCheck />;
+  let recommendationText = 'I recommend this product';
+
 
   return (
     <div className={styles.reviewContainer}>
       <span className={styles.starRating}><StarRating starRating={rating}/></span>
-      <span className={styles.datePosted}>{moment(review.date).format("ll")}</span>
-      <p className={styles.boldedSummary}>{boldedSummary}</p>
-      <p>{restOfSummary}</p>
-      <p>{body}</p>
-      <span className={styles.recommendation}>I recommend this product</span>
+      <span className={styles.datePosted}>{`${moment(review.date).format("ll")}`}</span>
+      <div className={styles.boldedSummary}>{boldedSummary} <p>{restOfSummary}</p></div>
+      <p className={styles.body}>{body}</p>
+      <span className={styles.recommendation}>{!review.recommend ? null : <span>
+        <FaCheck/> {recommendationText} </span>}</span>
     </div>
   )
 }
