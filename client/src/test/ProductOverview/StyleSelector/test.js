b93b2d9a2b75e38756e16f2797b475bd9c3643b7
Leslie Rodriguez <rleslie1015@gmail.com>
2021-04-22 00:15:25 -0500

    removes commented code

--- client/src/test/ProductOverview/StyleSelector/test.js
+++ client/src/test/ProductOverview/StyleSelector/test.js
@@ -19,17 +19,6 @@ describe('<StyleSelector />', () => {
     expect(wrapper.find('#style-wrap')).to.have.lengthOf(1);
   })
 
-
-
-  //TODOS: Refactor using a mock callback function to test click event instead
-  // it ('renders four reviews when button the "More Reviews" button is clicked', () => {
-  //   const wrapper = shallow(<Reviews reviews={dummyReviews.results}/>);
-  //   wrapper.setState({
-  //     clickedMoreReviews: true,
-  //     reviewsView: 4
-  //   });
-  //   expect(wrapper.find(ReviewItem)).toHaveLength(4);
-  // })
 });
 
 
